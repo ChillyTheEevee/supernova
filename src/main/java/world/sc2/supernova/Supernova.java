668fbc7a577730cba3348fa28cedfa0e39a08438
ChillyTheEevee <103384393+ChillyTheEevee@users.noreply.github.com>
2023-06-10 14:53:31 -0500

    Created the Supernova plugin and set up a basic plugin.yml file

---
+++ src/main/java/world/sc2/supernova/Supernova.java
@@ -0,0 +1,16 @@
+package world.sc2.supernova;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Supernova extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
